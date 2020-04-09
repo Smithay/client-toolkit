1a374786c119cfdad0d2b58e485a870ba2bc7522
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-09 09:26:02 +0000

    Deploy Smithay/client-toolkit to github.com/Smithay/client-toolkit.git:gh-pages

---
+++ aliases.js
@@ -0,0 +1,2 @@
+var ALIASES = {};
+ALIASES["smithay_client_toolkit"] = {};
---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["smithay_client_toolkit"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"smithay_client_toolkit/enum.DispatchError.html\" title=\"enum smithay_client_toolkit::DispatchError\">DispatchError</a>","synthetic":false,"types":["smithay_client_toolkit::event_loop::DispatchError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
