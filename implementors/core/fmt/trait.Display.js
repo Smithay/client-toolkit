48f1ab237cd8ff3b4ad9bf511cfd0a180d7c0a56
vberger <vberger@users.noreply.github.com>
2020-04-22 15:52:13 +0000

    deploy: 75cd901705a8404fa7d536b9da458a7d81aea659

--- implementors/core/fmt/trait.Display.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["smithay_client_toolkit"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"smithay_client_toolkit/enum.DispatchError.html\" title=\"enum smithay_client_toolkit::DispatchError\">DispatchError</a>","synthetic":false,"types":["smithay_client_toolkit::event_loop::DispatchError"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
