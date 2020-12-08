719244e05738ca71514b06597367e0bf2c54aad6
vberger <vberger@users.noreply.github.com>
2020-12-08 08:22:16 +0000

    deploy: 4a9e266c2ec473093f8d7d36c7b2f73c6300253d

--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["smithay_client_toolkit"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"smithay_client_toolkit/data_device/struct.ReadPipe.html\" title=\"struct smithay_client_toolkit::data_device::ReadPipe\">ReadPipe</a>","synthetic":false,"types":["smithay_client_toolkit::data_device::offer::ReadPipe"]}];
+implementors["smithay_client_toolkit"] = [{"text":"impl Read for ReadPipe","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/io/trait.Seek.js
+++ implementors/std/io/trait.Seek.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["smithay_client_toolkit"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Seek.html\" title=\"trait std::io::Seek\">Seek</a> for <a class=\"struct\" href=\"smithay_client_toolkit/shm/struct.MemPool.html\" title=\"struct smithay_client_toolkit::shm::MemPool\">MemPool</a>","synthetic":false,"types":["smithay_client_toolkit::shm::mempool::MemPool"]}];
+implementors["smithay_client_toolkit"] = [{"text":"impl Seek for MemPool","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
