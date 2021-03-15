ae6e6226b4dc47fc5744d918766b4bb430679585
vberger <vberger@users.noreply.github.com>
2021-03-15 13:45:03 +0000

    deploy: 559e00e07c234f3559587148a611bdc1c647cee5

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["smithay_client_toolkit"] = [{"text":"impl Display for MissingGlobal","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
