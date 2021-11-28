a5e845f4e5244fc4e50dab4a898300777e8d8097
i509VCB <git@i509.me>
2021-11-28 00:02:26 -0600

    Nuke existing SCTK codebase.
    0.30 is a complete rewrite pretty much.

--- src/lazy_global.rs
+++
@@ -1,9 +0,0 @@
-use wayland_client::{Attached, Interface};
-
-/// An utility for lazy-loading globals.
-#[derive(Debug)]
-pub enum LazyGlobal<I: Interface> {
-    Unknown,
-    Seen { id: u32, version: u32 },
-    Bound(Attached<I>),
-}
