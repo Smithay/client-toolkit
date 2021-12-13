ca8992168c2c9c613f3b5fbf947f24c465a0c910
i509VCB <git@i509.me>
2021-12-12 18:43:14 -0600

    WIP example for outputs

--- src/lib.rs
+++ src/lib.rs
@@ -12,30 +12,7 @@ pub mod reexports {
     pub use wayland_protocols as protocols;
 }
 
+pub mod compositor;
 pub mod output;
-
-/// TODO: Replace this wil wayland-rs delegate_dispatch when it supports fields.
-#[macro_export]
-macro_rules! delegate_dispatch {
-    ($dispatch_from: ty: [$($interface: ty),*] => $dispatch_to: ty ; |$dispatcher: ident| $closure: block) => {
-        $(
-            impl $crate::Dispatch<$interface> for $dispatch_from {
-                type UserData = <$dispatch_to as $crate::DelegateDispatchBase<$interface>>::UserData;
-
-                fn event(
-                    &mut self,
-                    proxy: &$interface,
-                    event: <$interface as $crate::Proxy>::Event,
-                    data: &Self::UserData,
-                    cxhandle: &mut $crate::ConnectionHandle,
-                    qhandle: &$crate::QueueHandle<Self>,
-                    init: &mut $crate::DataInit<'_>,
-                ) {
-                    let $dispatcher = self; // We need to do this so the closure can see the dispatcher field.
-                    let delegate: &mut $dispatch_to = { $closure };
-                    delegate.event(proxy, event, data, cxhandle, qhandle, init)
-                }
-            }
-        )*
-    };
-}
+pub mod shell;
+pub mod shm;
