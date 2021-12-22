3aae6a8f9cd676dcc1150555231f99e963b4a749
i509VCB <git@i509.me>
2021-12-21 19:18:15 -0600

    rework shm to use registry helpers

--- examples/list_shm_formats.rs
+++ examples/list_shm_formats.rs
@@ -1,12 +1,16 @@
-use smithay_client_toolkit::shm::{ShmDispatch, ShmHandler, ShmState};
+use smithay_client_toolkit::{
+    registry::{RegistryDispatch, RegistryHandle, RegistryHandler},
+    shm::{ShmDispatch, ShmHandler, ShmState},
+};
 use wayland_client::{
     delegate_dispatch,
     protocol::{wl_registry, wl_shm},
-    Connection, Dispatch,
+    Connection,
 };
 
 struct ListShmFormats {
     inner: InnerApp,
+    registry_handle: RegistryHandle,
     shm_state: ShmState,
 }
 
@@ -26,8 +30,12 @@ fn main() {
     let mut event_queue = cx.new_event_queue();
     let qh = event_queue.handle();
 
-    let _registry = display.get_registry(&mut cx.handle(), &qh, ()).unwrap();
-    let mut list_formats = ListShmFormats { inner: InnerApp, shm_state: ShmState::new() };
+    let registry = display.get_registry(&mut cx.handle(), &qh, ()).unwrap();
+    let mut list_formats = ListShmFormats {
+        inner: InnerApp,
+        registry_handle: RegistryHandle::new(registry),
+        shm_state: ShmState::new(),
+    };
 
     event_queue.blocking_dispatch(&mut list_formats).unwrap();
     println!("Supported formats:");
@@ -38,39 +46,8 @@ delegate_dispatch!(ListShmFormats: <UserData = ()> [wl_shm::WlShm] => ShmDispatc
     &mut ShmDispatch(&mut app.shm_state, &mut app.inner)
 });
 
-impl Dispatch<wl_registry::WlRegistry> for ListShmFormats {
-    type UserData = ();
+delegate_dispatch!(ListShmFormats: <UserData = ()> [wl_registry::WlRegistry] => RegistryDispatch<'_, ListShmFormats> ; |app| {
+    let handles: Vec<&mut dyn RegistryHandler<ListShmFormats>> = vec![&mut app.shm_state];
 
-    fn event(
-        &mut self,
-        registry: &wl_registry::WlRegistry,
-        event: wl_registry::Event,
-        _: &Self::UserData,
-        cx: &mut wayland_client::ConnectionHandle,
-        qh: &wayland_client::QueueHandle<Self>,
-        _: &mut wayland_client::DataInit<'_>,
-    ) {
-        match event {
-            wl_registry::Event::Global { name, interface, .. } => {
-                match &interface[..] {
-                    "wl_shm" => {
-                        let wl_shm = registry
-                            .bind::<wl_shm::WlShm, _>(cx, name, 1, qh, ())
-                            .expect("Failed to bind global");
-
-                        // Very temporary hack.
-                        #[allow(deprecated)]
-                        self.shm_state.shm_bind(wl_shm);
-                        println!("Bind [{}] {} @ v{}", name, &interface, 1);
-                    }
-
-                    _ => (),
-                }
-            }
-
-            wl_registry::Event::GlobalRemove { name: _ } => todo!(),
-
-            _ => unreachable!(),
-        }
-    }
-}
+    &mut RegistryDispatch(&mut app.registry_handle, handles)
+});
