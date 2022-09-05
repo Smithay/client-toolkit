768cf6b0389da31e5cae236f6fa58be2776cda1c
i509VCB <i509VCB@users.noreply.github.com>
2022-09-05 00:13:55 +0000

    deploy: e45bf693263afa90ac6882a3066311be52be7fb6

--- smithay_client_toolkit/output/sidebar-items.js
+++ smithay_client_toolkit/output/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["Mode",""],["OutputData",""],["OutputInfo","Information about an output."],["OutputState",""],["ScaleWatcherHandle",""]],"trait":[["OutputHandler",""]]};
+window.SIDEBAR_ITEMS = {"struct":[["Mode",""],["OutputData",""],["OutputInfo","Information about an output."],["OutputState","A handler for delegating `wl_output::WlOutput`."],["ScaleWatcherHandle",""]],"trait":[["OutputHandler","Simplified event handler for [`wl_output::WlOutput`]. See [`OutputState`]."]]};
