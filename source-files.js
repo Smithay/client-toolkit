ca91cef7af71d5c8f30e56b1808a2a1475eaa27f
i509VCB <i509VCB@users.noreply.github.com>
2022-09-23 04:01:34 +0000

    deploy: 1cc11af8538040f46173bf021ac079c342ddeebb

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["smithay_client_toolkit"] = {"name":"","dirs":[{"name":"seat","dirs":[{"name":"keyboard","files":["keysyms.rs","mod.rs"]}],"files":["mod.rs","pointer.rs","touch.rs"]},{"name":"shell","dirs":[{"name":"layer","files":["dispatch.rs","mod.rs"]},{"name":"xdg","dirs":[{"name":"window","files":["inner.rs","mod.rs"]}],"files":["inner.rs","mod.rs","popup.rs"]}],"files":["mod.rs"]},{"name":"shm","files":["mod.rs","multi.rs","raw.rs","slot.rs"]}],"files":["compositor.rs","error.rs","event_loop.rs","globals.rs","lib.rs","output.rs","registry.rs"]};
+var sourcesIndex = JSON.parse('{\
+"smithay_client_toolkit":["",[["seat",[["keyboard",[],["keysyms.rs","mod.rs"]]],["mod.rs","pointer.rs","touch.rs"]],["shell",[["layer",[],["dispatch.rs","mod.rs"]],["xdg",[["window",[],["inner.rs","mod.rs"]]],["inner.rs","mod.rs","popup.rs"]]],["mod.rs"]],["shm",[],["mod.rs","multi.rs","raw.rs","slot.rs"]]],["compositor.rs","error.rs","event_loop.rs","globals.rs","lib.rs","output.rs","registry.rs"]]\
+}');
 createSourceSidebar();
