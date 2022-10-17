28af60e2b2f53cfbc6e2b95a2b258c2dcbc13ad3
i509VCB <i509VCB@users.noreply.github.com>
2022-10-17 14:09:37 +0000

    deploy: 518e77b0dfc4fa648fe803a5a877a3735a1ff6a9

--- smithay_client_toolkit/shell/xdg/sidebar-items.js
+++ smithay_client_toolkit/shell/xdg/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["popup",""],["window",""]],"struct":[["XdgPositioner","A trivial wrapper for an [`xdg_positioner::XdgPositioner`]."],["XdgShellState",""],["XdgShellSurface",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["popup",""],["window",""]],"struct":[["XdgPositioner","A trivial wrapper for an [`xdg_positioner::XdgPositioner`]."],["XdgShellState",""],["XdgShellSurface",""]],"trait":[["XdgShellHandler",""]]};
