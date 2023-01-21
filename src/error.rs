8779517b1f9017e24e67173115694edc542f1d2b
Ian Douglas Scott <idscott@system76.com>
2023-01-20 19:01:24 -0800

    Remove `GlobalProxy::NotReady` variant
    This doesn't seem to serve any role after
    https://github.com/Smithay/client-toolkit/pull/313.

--- src/error.rs
+++ src/error.rs
@@ -8,8 +8,4 @@ pub enum GlobalError {
     /// A compositor global was available, but did not support the given minimum version
     #[error("the '{name}' global does not support interface version {required} (using version {available})")]
     InvalidVersion { name: &'static str, required: u32, available: u32 },
-
-    /// This is likely a programming error due to a missing entry in the registry_handlers! macro.
-    #[error("global enumeration is not finished or missing ProvidesRegistryState delegation")]
-    NotReady,
 }
