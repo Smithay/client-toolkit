c954ee7036cd202a488e151f1c037cecc9026134
wash2 <wash2@users.noreply.github.com>
2023-01-21 21:35:51 +0000

    deploy: beefe0a7078727654af48484e03bdfd19dfff8f3

--- smithay_client_toolkit/seat/sidebar-items.js
+++ smithay_client_toolkit/seat/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["Capability",""],["SeatError",""]],"mod":[["keyboard",""],["pointer",""],["touch",""]],"struct":[["SeatData",""],["SeatInfo","Description of a seat."],["SeatState",""]],"trait":[["SeatHandler",""]]};
+window.SIDEBAR_ITEMS = {"enum":[["Capability",""],["SeatError",""]],"mod":[["keyboard",""],["pointer",""],["relative_pointer",""],["touch",""]],"struct":[["SeatData",""],["SeatInfo","Description of a seat."],["SeatState",""]],"trait":[["SeatHandler",""]]};
