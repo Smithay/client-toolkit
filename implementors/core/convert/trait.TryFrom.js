c55c029946832fd3514b6d9fdab7c882ad6b5217
wash2 <wash2@users.noreply.github.com>
2023-02-10 03:28:40 +0000

    deploy: e97ad6ddf1f9fc6c82efee8a133ec15c66c9ad8d

--- implementors/core/convert/trait.TryFrom.js
+++ implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"smithay_client_toolkit":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;Layer&gt; for <a class=\"enum\" href=\"smithay_client_toolkit/shell/layer/enum.Layer.html\" title=\"enum smithay_client_toolkit::shell::layer::Layer\">Layer</a>"]]
+"smithay_client_toolkit":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;Layer&gt; for <a class=\"enum\" href=\"smithay_client_toolkit/shell/layer/enum.Layer.html\" title=\"enum smithay_client_toolkit::shell::layer::Layer\">Layer</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
