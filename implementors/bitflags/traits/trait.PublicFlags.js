98000abdd0e98de735b2a69b30ae7f3897496e32
wash2 <wash2@users.noreply.github.com>
2023-09-22 03:46:56 +0000

    deploy: a77fef4463c2225a294150cfc3bc2fc3d5a4a19b

---
+++ implementors/bitflags/traits/trait.PublicFlags.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"smithay_client_toolkit":[["impl PublicFlags for <a class=\"struct\" href=\"smithay_client_toolkit/shell/wlr_layer/struct.Anchor.html\" title=\"struct smithay_client_toolkit::shell::wlr_layer::Anchor\">Anchor</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
