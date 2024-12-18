5dcd718aafc8df4968e5898e71e1fb8587f30572
wash2 <wash2@users.noreply.github.com>
2024-12-18 00:14:17 +0000

    deploy: ca6a36e4d5f4a171eea91eabe7c8ab9b17fa98e0

--- trait.impl/std/os/fd/owned/trait.AsFd.js
+++ trait.impl/std/os/fd/owned/trait.AsFd.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["smithay_client_toolkit",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"smithay_client_toolkit/data_device_manager/struct.ReadPipe.html\" title=\"struct smithay_client_toolkit::data_device_manager::ReadPipe\">ReadPipe</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"smithay_client_toolkit/data_device_manager/struct.WritePipe.html\" title=\"struct smithay_client_toolkit::data_device_manager::WritePipe\">WritePipe</a>"]]]]);
+    var implementors = Object.fromEntries([["smithay_client_toolkit",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"smithay_client_toolkit/data_device_manager/struct.ReadPipe.html\" title=\"struct smithay_client_toolkit::data_device_manager::ReadPipe\">ReadPipe</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"smithay_client_toolkit/data_device_manager/struct.WritePipe.html\" title=\"struct smithay_client_toolkit::data_device_manager::WritePipe\">WritePipe</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"smithay_client_toolkit/shm/raw/struct.RawPool.html\" title=\"struct smithay_client_toolkit::shm::raw::RawPool\">RawPool</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[697]}
+//{"start":57,"fragment_lengths":[1004]}
