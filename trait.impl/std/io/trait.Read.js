e183b4ffdddf199251b2a08d80f076c44bb4fd8c
wash2 <wash2@users.noreply.github.com>
2026-07-20 15:56:14 +0000

    deploy: 766ba9e9c08afe74434b81ce694321bdea43c0d3

--- trait.impl/std/io/trait.Read.js
+++ trait.impl/std/io/trait.Read.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["smithay_client_toolkit",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"smithay_client_toolkit/data_device_manager/struct.ReadPipe.html\" title=\"struct smithay_client_toolkit::data_device_manager::ReadPipe\">ReadPipe</a>",0]]]]);
+    const implementors = Object.fromEntries([["smithay_client_toolkit",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"smithay_client_toolkit/data_device_manager/struct.ReadPipe.html\" title=\"struct smithay_client_toolkit::data_device_manager::ReadPipe\">ReadPipe</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
